2350f741f51e019cebf7a09396b853a9ee75e80c
IngMar11 <101218252+IngMar11@users.noreply.github.com>
2024-02-20 17:35:13 +0100

    Update shift_register_tb.vhd

--- Exercise_4_1/shift_register_tb.vhd
+++ Exercise_4_1/shift_register_tb.vhd
@@ -15,11 +15,12 @@ architecture tb of shift_register_tb is
               Q  : out std_logic_vector);
     end component;
 
+    constant N: natural :=8;
     signal clk_tb : std_logic;
     signal input  : std_logic :='0';
-    signal output : std_logic_vector (0 to 32);
+    signal output : std_logic_vector ((N-1) downto 0);
     signal rst : std_logic;
-    signal mode: std_logic_vector (0 to 1) := (others => '0');
+    signal mode: std_logic_vector (1 downto 0) := (others => '0');
 
     constant clk_period : time := 20 ns; 
     
@@ -35,36 +36,66 @@ begin
    end process;
    
    
-   
     uut : SR
     GENERIC MAP (N => 8)
     port map (CLOCK => clk_tb,
               RST => rst,
               MODE => mode,
               D  => input,
-              Q  => output(0 to 7));
+              Q  => output(N-1 downto 0));
               
     stimuli : process
     begin
         
         rst <= '1';
-        mode <="10";
-        wait for 100ns;  --global reset
-       
-        rst <='0';
-        input <= '1';
+        mode <="00";
         wait for 50ns;
-        mode <= "01";
-        wait for 50ns;
-        input <= '0';
-        wait for 100ns;
-        input <= '1';
+        rst <='0';      
         
-        -- EDIT Add stimuli here
-        --wait for 100 * TbPeriod;
-
-        -- Stop the clock and hence terminate the simulation
-       -- TbSimEnded <= '1';
+        input <= '1';
+        wait for 60ns;
+        input <= '0';
+        wait for 60ns;
+        input <= '1';
+        wait for 30ns;
+        
+        rst <= '1';
+        mode <="01";
+        wait for 50ns;
+        rst <='0';
+        
+        input <= '1';
+        wait for 60ns;
+        input <= '0';
+        wait for 60ns;
+        input <= '1';
+        wait for 30ns;
+        
+        rst <= '1';
+        mode <="10";
+        wait for 50ns;
+        rst <='0';
+        
+        input <= '1';
+        wait for 60ns;
+        input <= '0';
+        wait for 60ns;
+        input <= '1';
+        wait for 30ns;
+        
+        
+        rst <= '1';
+        mode <="11";
+        wait for 50ns;
+        rst <='0';
+        
+        input <= '1';
+        wait for 60ns;
+        input <= '0';
+        wait for 60ns;
+        input <= '1';
+        wait for 30ns;
+        
         wait;
     end process;
 
